c695211bc642f2367e1fcd22e8b01fb6024b86e5
Aldii24 <aldiirawan748@gmail.com>
2024-05-21 16:04:03 +0700

    personal

--- src/components/NavSocials/page.jsx
+++ src/components/NavSocials/page.jsx
@@ -6,23 +6,30 @@ import {
 import Link from "next/link";
 
 const NavSocials = () => {
+  const facebook = "https://www.facebook.com/aldi.irawan.311493/";
+  const whatsapp = "https://wa.me/6281372648563";
+  const instagram = "https://www.instagram.com/allrvvnn/";
+
   return (
     <div>
       <div className="flex md:gap-4 gap-1 mt-2">
-        <Link href={"/"}>
+        <Link href={facebook}>
           <FacebookLogo
             size={25}
             className="text-gray-800 hover:bg-color-hover dark:text-white rounded-full hover:text-color-text transition-all"
           />
         </Link>
-        <Link href={"/"}>
+        <Link href={instagram}>
           <InstagramLogo
             size={25}
             className="text-gray-800 hover:bg-color-hover dark:text-white rounded-full hover:text-color-text transition-all"
           />
         </Link>
-        <Link href={"/"}>
-          <WhatsappLogo size={25} className="text-gray-800 dark:text-white hover:bg-color-hover rounded-full hover:text-color-text transition-all" />
+        <Link href={whatsapp}>
+          <WhatsappLogo
+            size={25}
+            className="text-gray-800 dark:text-white hover:bg-color-hover rounded-full hover:text-color-text transition-all"
+          />
         </Link>
       </div>
     </div>
